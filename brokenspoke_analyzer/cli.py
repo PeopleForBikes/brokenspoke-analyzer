7a2da27aaa5afe95133b778ef0c437d07f36a2aa
Rémy Greinhofer <remy.greinhofer@gmail.com>
2022-08-07 11:54:37 -0500

    Prepare for 1.0.0-rc.1 release
    Prepares the codebase for the 1.0.0-rc.1 release.
    
    The main changes are related to documentation and making it easier to
    involve the community.
    
    Drive-by:
    - Fixes #27

--- brokenspoke_analyzer/cli.py
+++ brokenspoke_analyzer/cli.py
@@ -4,13 +4,11 @@ import pathlib
 import sys
 from typing import Optional
 
-import aiohttp
 import geopandas as gpd
 import typer
 from loguru import logger
 from pyrosm import get_data
 from rich.console import Console
-from slugify import slugify
 
 from brokenspoke_analyzer.core import analysis
 from brokenspoke_analyzer.core import processhelper
@@ -80,6 +78,10 @@ def run(
 
 async def prepare_and_run(country, state, city, output_dir):
     """Prepare and run an analysis."""
+    speed_file = output_dir / "city_fips_speed.csv"
+    speed_file.unlink(missing_ok=True)
+    tabblock_file = output_dir / "tabblock2010_91_pophu.zip"
+    tabblock_file.unlink(missing_ok=True)
     params = await prepare_(country, state, city, output_dir)
     analyze_(*params)
 
@@ -126,22 +128,30 @@ async def prepare_(country, state, city, output_dir):
     # For non-US cities:
     if state_fips == MAGIC_STATE_NUMBER:
         # Create synthetic population.
-        CELL_SIZE = (1000, 1000)
-        city_boundaries_gdf = gpd.read_file(city_shp)
-        synthetic_population = analysis.create_synthetic_population(
-            city_boundaries_gdf, *CELL_SIZE
-        )
+        with console.status("[bold green]Prepare synthetic population..."):
+            CELL_SIZE = (1000, 1000)
+            city_boundaries_gdf = gpd.read_file(city_shp)
+            synthetic_population = analysis.create_synthetic_population(
+                city_boundaries_gdf, *CELL_SIZE
+            )
+            console.log("Synthetic population ready.")
 
         # Simulate the census blocks.
-        analysis.simulate_census_blocks(
-            output_dir, slug, state_fips, synthetic_population
-        )
+        with console.status("[bold green]Simulate census blocks..."):
+            analysis.simulate_census_blocks(
+                output_dir, slug, state_fips, synthetic_population
+            )
+            console.log("Census blocks ready.")
 
         # Change the speed limit.
-        DEFAULT_SPEED_LIMIT_KMH = 50
-        analysis.change_speed_limit(
-            output_dir, city, state_abbrev, DEFAULT_SPEED_LIMIT_KMH
-        )
+        with console.status("[bold green]Adjust default speed limit..."):
+            DEFAULT_SPEED_LIMIT_KMH = 50
+            analysis.change_speed_limit(
+                output_dir, city, state_abbrev, DEFAULT_SPEED_LIMIT_KMH
+            )
+            console.log(
+                f"Default speed limit adjusted to {DEFAULT_SPEED_LIMIT_KMH} km/h."
+            )
 
     # Return the parameters required to perform the analysis.
     # pylint: disable=duplicate-code
@@ -173,64 +183,3 @@ def analyze_(
             output_dir,
         )
         console.log(f"Analysis for {city_shp} complete.")
-
-
-async def _prepare_with_osmrelationid(state, city, osm_relation_id, output_dir):
-    # pylint: disable=too-many-locals
-    """Prepare and kicks off the analysis."""
-    # Prepare the Rich output.
-    console = Console()
-
-    # Computed inputs.
-    region_file_name = f"{slugify(state)}-latest.osm.pbf"
-    slugged_city_state = slugify(f"{city}-{state}")
-    polygon_file_name = f"{slugged_city_state}.poly"
-    pfb_osm_file = f"{slugged_city_state}.osm"
-    state_abbrev, state_fips = analysis.state_info(state)
-
-    # Retrieve the US Census file.
-    async with aiohttp.ClientSession(raise_for_status=True) as session:
-        # Download the census file.
-        with console.status("[bold green]Downloading the US census file..."):
-            tiger_file = await analysis.download_census_file(
-                session, output_dir, state_fips
-            )
-            console.log("US Census file downloaded.")
-
-        # Prepare the boudary file.
-        with console.status("[bold green]Preparing the boudary file..."):
-            city_shp = analysis.prepare_boundary_file(output_dir, city, tiger_file)
-            console.log("Boundary file ready.")
-
-        # Download the OSM region file.
-        with console.status("[bold green]Downloading the US OSM file..."):
-            region_file_path = await analysis.download_osm_us_region_file(
-                session, output_dir, state, region_file_name
-            )
-            console.log("Regional OSM file downloaded.")
-
-        # Download the polygon file.
-        with console.status("[bold green]Downloading the polygon file..."):
-            polygon_file_path = output_dir / polygon_file_name
-            await analysis.download_polygon_file(
-                session, osm_relation_id, polygon_file_path
-            )
-            console.log("Polygon file downloaded.")
-
-    # Reduce the osm file with osmium.
-    # Osmium performs way better than osmosis.
-    with console.status(f"[bold green]Reducing the OSM file for {city} with osmium..."):
-        analysis.prepare_city_file(
-            output_dir, region_file_path, polygon_file_path, pfb_osm_file
-        )
-        console.log(f"OSM file for {city} ready.")
-
-    # Return the parameters required to perform the analysis.
-    # pylint: disable=duplicate-code
-    return (
-        state_abbrev,
-        state_fips,
-        city_shp,
-        pfb_osm_file,
-        output_dir,
-    )
