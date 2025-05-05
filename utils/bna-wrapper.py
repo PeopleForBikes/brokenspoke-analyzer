1a531ad02ea48c7a3fef350627533c5b6f34686b
Rémy Greinhofer <remy.greinhofer@gmail.com>
2025-05-05 08:37:24 -0500

    Implement a caching-like mechanism for OSM data.
    Signed-off-by: Rémy Greinhofer <remy.greinhofer@gmail.com>

--- utils/bna-wrapper.py
+++ utils/bna-wrapper.py
@@ -17,12 +17,18 @@ uv run python utils/bna-wrapper.py
 import csv
 import os
 import pathlib
+import shutil
+
+import rich
 
 from brokenspoke_analyzer.cli import (
     root,
     run_with,
 )
-from brokenspoke_analyzer.core import constant
+from brokenspoke_analyzer.core import (
+    analysis,
+    constant,
+)
 
 
 def main():
@@ -36,6 +42,13 @@ def main():
     # Enable cache.
     os.environ["BNA_CACHING_STRATEGY"] = "USER_CACHE"
 
+    # Simulate a caching mechanism for OSM data.
+    osm_cache = pathlib.Path("./osm_cache").resolve()
+    osm_cache.mkdir(parents=True, exist_ok=True)
+
+    # Prepare the Rich output.
+    console = rich.get_console()
+
     # Read the CSV file.
     input_csv = pathlib.Path("cities.csv")
     with input_csv.open() as f:
@@ -45,8 +58,34 @@ def main():
         for row in reader:
             country = row["country"]
             city = row["city"]
-            region = row["region"]
+            region = row.get("region")
             fips_code = row["fips_code"]
+
+            # Download the OSM data into the cache if necessary.
+            console.log(
+                f"[green]Caching the OSM region file for {region}...",
+            )
+            with console.status("Downloading..."):
+                osm_region = region if region else country
+                try:
+                    cached_region_file = analysis.retrieve_region_file(
+                        osm_region, osm_cache
+                    )
+                    cached_region_file_md5 = cached_region_file.with_suffix(".pbf.md5")
+                except Exception as e:
+                    print(e)
+
+            # Prepare the data directory ahead of the analysis.
+            data_dir = pathlib.Path("./data").resolve()
+            _, slug = analysis.osmnx_query(country, city, region)
+            data_dir /= slug
+            data_dir.mkdir(parents=True, exist_ok=True)
+
+            # Copy the OSM data into the data directory.
+            shutil.copy(cached_region_file, data_dir)
+            shutil.copy(cached_region_file_md5, data_dir)
+
+            # Run the analysis.
             run_with.compose(
                 country=country,
                 city=city,
