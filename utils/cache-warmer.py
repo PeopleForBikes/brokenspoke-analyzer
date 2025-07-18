79daa1b2a9b64b4a8f447b6687139873e86a3c98
Rémy Greinhofer <remy.greinhofer@gmail.com>
2025-07-18 10:01:00 -0500

    Add new utilities (#891)
    Adds new utilities to warn the caches with files from the 2020 Census
    and the 2022 LODES data.
    
    Adds a new utilityes to sync up the Cloudflare R2 cache bucket with
    the AWS S3 cache bucket.
    
    Signed-off-by: Rémy Greinhofer <remy.greinhofer@gmail.com>

--- utils/cache-warmer.py
+++ utils/cache-warmer.py
@@ -11,6 +11,8 @@ The cache will be populated with the following items:
     - US City speed limits
 """
 
+from __future__ import annotations
+
 import asyncio
 import os
 import pathlib
@@ -55,5 +57,31 @@ async def main():
             await bna_store.fetch_to_cache(session, url, tabblk2010_zip)
 
 
+async def main_2020():
+    """Define the main function for  caching the 2020 files."""
+    bna_store = datastore.BNADataStore(
+        pathlib.Path(utils.get_user_cache_dir()),
+        # datastore.CacheType.USER_CACHE,
+        datastore.CacheType.AWS_S3,
+        s3_bucket=os.getenv("BNA_CACHE_AWS_S3_BUCKET"),
+    )
+
+    async with aiohttp.ClientSession() as session:
+        for fips, abbr in us.states.mapping("fips", "abbr").items():
+            logger.info(f"Downloading LODES data for {abbr} ({fips})")
+            lehd_url = (
+                f"https://lehd.ces.census.gov/data/lodes/LODES8/{abbr.lower()}/od"
+            )
+            for part in ["main", "aux"]:
+                lehd_gz = f"{abbr.lower()}_od_{part.lower()}_JT00_2022.csv.gz"
+                url = f"{lehd_url}/{lehd_gz}"
+                await bna_store.fetch_to_cache(session, url, lehd_gz)
+
+            logger.info(f"Downloading census data for {abbr} ({fips})")
+            tabblk2020_zip = f"tl_2020_{fips}_tabblock20.zip"
+            url = f"https://www2.census.gov/geo/tiger/TIGER2020/TABBLOCK20/{tabblk2020_zip}"
+            await bna_store.fetch_to_cache(session, url, tabblk2020_zip)
+
+
 if __name__ == "__main__":
-    asyncio.run(main())
+    asyncio.run(main_2020())
