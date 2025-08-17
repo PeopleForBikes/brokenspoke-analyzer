99c0f32550ab2a82f95f4e438528b2c7e1b793b0
Rémy Greinhofer <remy.greinhofer@gmail.com>
2025-08-17 11:15:07 -0500

    Improve CLI and documentation (#899)
    * Improve CLI and documentation
    
    Improve the CLI and its documentation.
    
    Fixes PeopleForBikes/brokenspoke-analyzer#866
    Fixes PeopleForBikes/brokenspoke-analyzer#897
    
    Signed-off-by: Rémy Greinhofer <remy.greinhofer@gmail.com>
    
    * Update brokenspoke_analyzer/cli/prepare.py
    
    Co-authored-by: Luis Alvergue <lalver1@gmail.com>
    
    * Update brokenspoke_analyzer/cli/run_with.py
    
    Co-authored-by: Luis Alvergue <lalver1@gmail.com>
    
    * Update brokenspoke_analyzer/cli/run_with.py
    
    Co-authored-by: Luis Alvergue <lalver1@gmail.com>
    
    * Update brokenspoke_analyzer/cli/run_with.py
    
    Co-authored-by: Luis Alvergue <lalver1@gmail.com>
    
    ---------
    
    Signed-off-by: Rémy Greinhofer <remy.greinhofer@gmail.com>
    Co-authored-by: Luis Alvergue <lalver1@gmail.com>

--- brokenspoke_analyzer/cli/root.py
+++ brokenspoke_analyzer/cli/root.py
@@ -1,35 +1,25 @@
 """Define the top-level commands."""
 
 import logging
-import pathlib
 import typing
 from importlib import (
     metadata,
-    resources,
 )
 
 import typer
 from loguru import logger
-from rich.console import Console
 from rich.logging import RichHandler
 from typing_extensions import Annotated
 
 from brokenspoke_analyzer.cli import (
-    common,
+    compute,
     configure,
     export,
     importer,
     prepare,
+    run,
     run_with,
 )
-from brokenspoke_analyzer.core import (
-    analysis,
-    compute,
-    exporter,
-    ingestor,
-    utils,
-)
-from brokenspoke_analyzer.core.database import dbcore
 
 # Create the CLI app.
 app = typer.Typer()
@@ -99,110 +89,15 @@ def callback(
 
 
 # Register the sub-commands.
+app.add_typer(compute.app, help="Compute the analysis results.")
 app.add_typer(
     configure.app, name="configure", help="Configure a database for an analysis."
 )
-app.add_typer(prepare.app, name="prepare", help="Prepare files needed for an analysis.")
-app.add_typer(importer.app, name="import", help="Import files into database.")
 app.add_typer(export.app, name="export", help="Export tables from database.")
+app.add_typer(importer.app, name="import", help="Import files into database.")
+app.add_typer(prepare.app, help="Prepare files needed for an analysis.")
+app.add_typer(run.app, help="Run a full analysis.")
 app.add_typer(run_with.app, name="run-with", help="Run an analysis in different ways.")
 
 # Make shared options accessible to appropriate subcommands.
 run_with.verbose = verbose
-
-
-@app.command(name="compute")
-def compute_cmd(
-    database_url: common.DatabaseURL,
-    input_dir: common.InputDir,
-    country: common.Country,
-    city: common.City,
-    region: common.Region = None,
-    buffer: common.Buffer = common.DEFAULT_BUFFER,
-    with_parts: common.ComputeParts = common.DEFAULT_COMPUTE_PARTS,
-) -> None:
-    """Run an analysis."""
-    # Make MyPy happy.
-    if not buffer:
-        raise ValueError("`buffer` must be set")
-
-    # Prepare the database connection.
-    engine = dbcore.create_psycopg_engine(database_url)
-
-    # Prepare directories.
-    country = utils.normalize_country_name(country)
-    _, _, slug = analysis.osmnx_query(country, city, region)
-    traversable = resources.files("brokenspoke_analyzer.scripts.sql")
-    res = pathlib.Path(traversable._paths[0])  # type: ignore
-    sql_script_dir = res.resolve(strict=True)
-    boundary_file = input_dir / f"{slug}.shp"
-
-    # Prepare compute params.
-    state_default_speed, city_default_speed = ingestor.retrieve_default_speed_limits(
-        engine
-    )
-    import_jobs = utils.is_usa(country)
-
-    # Compute the output SRID from the boundary file.
-    output_srid = utils.get_srid(boundary_file.resolve(strict=True))
-    logger.debug(f"{output_srid=}")
-
-    console = Console()
-    with console.status("[green]Running the full analysis (may take a while)..."):
-        compute.parts(
-            database_url=database_url,
-            sql_script_dir=sql_script_dir,
-            output_srid=output_srid,
-            buffer=buffer,
-            state_default_speed=state_default_speed,
-            city_default_speed=city_default_speed,
-            import_jobs=import_jobs,
-            compute_parts=with_parts,
-        )
-        console.log(f"Analysis for {slug} complete.")
-
-
-@app.command()
-def run(
-    database_url: common.DatabaseURL,
-    country: common.Country,
-    city: common.City,
-    region: common.Region = None,
-    output_dir: common.OutputDir = common.DEFAULT_OUTPUT_DIR,
-    fips_code: common.FIPSCode = common.DEFAULT_CITY_FIPS_CODE,
-    buffer: common.Buffer = common.DEFAULT_BUFFER,
-    city_speed_limit: common.SpeedLimit = common.DEFAULT_CITY_SPEED_LIMIT,
-    block_size: common.BlockSize = common.DEFAULT_BLOCK_SIZE,
-    block_population: common.BlockPopulation = common.DEFAULT_BLOCK_POPULATION,
-    lodes_year: common.LODESYear = common.DEFAULT_LODES_YEAR,
-    retries: common.Retries = common.DEFAULT_RETRIES,
-    max_trip_distance: common.MaxTripDistance = common.DEFAULT_MAX_TRIP_DISTANCE,
-    with_export: typing.Optional[exporter.Exporter] = exporter.Exporter.local,
-    s3_bucket: Annotated[
-        typing.Optional[str], typer.Option(help="S3 bucket name where to export")
-    ] = None,
-    s3_dir: typing.Optional[pathlib.Path] = None,
-    with_bundle: typing.Optional[bool] = False,
-    with_parts: common.ComputeParts = common.DEFAULT_COMPUTE_PARTS,
-) -> None:
-    """Run an analysis."""
-    run_with.run_(
-        database_url=database_url,
-        country=country,
-        city=city,
-        region=region,
-        output_dir=output_dir,
-        fips_code=fips_code,
-        buffer=buffer,
-        city_speed_limit=city_speed_limit,
-        block_size=block_size,
-        block_population=block_population,
-        lodes_year=lodes_year,
-        retries=retries,
-        max_trip_distance=max_trip_distance,
-        with_export=with_export,
-        s3_bucket=s3_bucket,
-        s3_dir=s3_dir,
-        with_bundle=with_bundle,
-        with_parts=with_parts,
-    )
