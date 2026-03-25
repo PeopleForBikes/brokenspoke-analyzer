75a4b77a16cf485aac87a959c313e765a8127892
Mitchell Henke <mitchell@mitchellhenke.com>
2026-03-24 19:18:09 -0500

    Exclude blocks with null cost from connected census blocks (#1031)
    Co-authored-by: Rémy Greinhofer <remy.greinhofer@gmail.com>
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- brokenspoke_analyzer/scripts/sql/connectivity/connected_census_blocks.sql
+++ brokenspoke_analyzer/scripts/sql/connectivity/connected_census_blocks.sql
@@ -19,31 +19,36 @@ INSERT INTO generated.neighborhood_connected_census_blocks (
     source_blockid20, target_blockid20,
     low_stress, low_stress_cost, high_stress, high_stress_cost
 )
-SELECT
-    source.geoid20, -- noqa: AL08
-    target.geoid20, -- noqa: AL08
-    FALSE, -- noqa: AL03
-    (
-        SELECT MIN(ls.total_cost)
-        FROM neighborhood_reachable_roads_low_stress AS ls
-        WHERE
-            ls.base_road = ANY(source.road_ids)
-            AND ls.target_road = ANY(target.road_ids)
-    ) AS min_ls_total_cost,
-    TRUE, -- noqa: AL03
-    (
-        SELECT MIN(hs.total_cost)
-        FROM neighborhood_reachable_roads_high_stress AS hs
-        WHERE
-            hs.base_road = ANY(source.road_ids)
-            AND hs.target_road = ANY(target.road_ids)
-    ) AS min_hs_total_cost
-FROM neighborhood_census_blocks AS source,
-    neighborhood_census_blocks AS target,
-    neighborhood_boundary
-WHERE
-    ST_Intersects(source.geom, neighborhood_boundary.geom)
-    AND ST_DWithin(source.geom, target.geom, :nb_max_trip_distance);
+WITH census_block_pairs AS (
+    SELECT
+        source.geoid20, -- noqa: AL08
+        target.geoid20, -- noqa: AL08
+        FALSE, -- noqa: AL03
+        (
+            SELECT MIN(ls.total_cost)
+            FROM neighborhood_reachable_roads_low_stress AS ls
+            WHERE
+                ls.base_road = ANY(source.road_ids)
+                AND ls.target_road = ANY(target.road_ids)
+        ) AS min_ls_total_cost,
+        TRUE, -- noqa: AL03
+        (
+            SELECT MIN(hs.total_cost)
+            FROM neighborhood_reachable_roads_high_stress AS hs
+            WHERE
+                hs.base_road = ANY(source.road_ids)
+                AND hs.target_road = ANY(target.road_ids)
+        ) AS min_hs_total_cost
+    FROM neighborhood_census_blocks AS source,
+        neighborhood_census_blocks AS target,
+        neighborhood_boundary
+    WHERE
+        ST_Intersects(source.geom, neighborhood_boundary.geom)
+        AND ST_DWithin(source.geom, target.geom, :nb_max_trip_distance)
+)
+
+SELECT * FROM census_block_pairs
+WHERE min_ls_total_cost IS NOT NULL OR min_hs_total_cost IS NOT NULL;
 
 -- set low_stress
 UPDATE generated.neighborhood_connected_census_blocks
