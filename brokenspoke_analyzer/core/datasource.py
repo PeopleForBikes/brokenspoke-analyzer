b4fdfb915107ffb80a070bab5c80581604456af0
Rémy Greinhofer <remy.greinhofer@gmail.com>
2026-07-18 13:34:40 -0500

    Use Datastore To Fetch City Boundaries
    Updates the logic to leverage the Datastore to fetch the city boundaries
    file and cache them.
    
    Fixes PeopleForBikes/brokenspoke-analyzer#1141
    
    Signed-off-by: Rémy Greinhofer <remy.greinhofer@gmail.com>

--- brokenspoke_analyzer/core/datasource.py
+++ brokenspoke_analyzer/core/datasource.py
@@ -218,14 +218,10 @@ class WorldPopAdapter(SourceAdapter):
     @property
     def urls(self) -> abc.Sequence[yarl.URL]:
         """Return the source data URLs."""
-        return [
-            self.source_url
-            / str(self.year)
-            / self.country
-            / "v1/1km_ua/constrained"
-            / str(f)
-            for f in self.files
-        ]
+        base_url = (
+            self.source_url / str(self.year) / self.country / "v1/1km_ua/constrained"
+        )
+        return [base_url / str(f) for f in self.files]
 
     def prepare(self, datastore: pathlib.Path) -> None:
         """Prepare the data files."""
